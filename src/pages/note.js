b7b9d911dee453e99cd2c4cb17827886713e6942
Bhavan Kamble <bhavan.Kamble3@gmail.com>
2020-12-25 19:29:56 +0530

    update-1

--- src/pages/note.js
+++ src/pages/note.js
@@ -4,9 +4,7 @@ import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 const Note = (props) => {
   return (
     <Card key={props.data.addedOn} xs={4} className="card">
-      <span className="close" onClick={props.deleteNote}>
-        X
-      </span>
+      <span className="close" onClick={props.deleteNote}></span>
       <CardHeader
         title={props.data.title}
         subheader={new Date(props.data.addedOn).toLocaleString()}
